b072c6721fd7066832056a95c1cc6cf12fc7d9c3
Michael Rezq Gerges <mrgerges@EFG-Hermes.com>
2023-10-01 14:48:10 +0300

    in the mango.web create the CouponController and inject the couponService, and add the CouponIndex Method,

---
+++ Mango.Web/Controllers/CouponControler.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Services.CouponAPI.Models.DTO;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponControler : Controller
+    {
+
+        private readonly ICouponService _couponService;
+        public CouponControler(ICouponService _couponService)
+        {
+            _couponService = _couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+            if (response != null && response.IsSuccess==true)
+            {
+               list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(response);
+        }
+    }
+}
