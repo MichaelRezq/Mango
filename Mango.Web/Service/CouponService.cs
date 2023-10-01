aef305357eebbf920d7cbb92067b0e771d3d7aef
Michael Rezq Gerges <mrgerges@EFG-Hermes.com>
2023-10-01 11:25:42 +0300

    to test all this, we need to call our CouponApi, so - we create ICouponService,.cs and CouponService.cs - copy the CouponDto to the Mango.Web.Models. - in the ICouponService add Inteface methods for each crud method. - implement the ICouponService methods inside CouponService.

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Services.CouponAPI.Models.DTO;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService:ICouponService
+    {
+        // the BaseService is responsable for handling any request type, so i will inject it here
+
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string CouponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
